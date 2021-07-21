2003bca0ed3a7999b2f1c226c60a40bd1bc54847
Kaleb Heitzman <kaleb@jamfire.io>
2021-07-20 22:25:36 -0400

    test(gatsby-theme-jamfire-conference-e2e): updated cookies tests
    Cookies test now checks that analytics cookies can bet set and unset using the manage cookies modal

--- packages/gatsby-theme-jamfire-conference-e2e/cypress/e2e/cookies.test.ts
+++ packages/gatsby-theme-jamfire-conference-e2e/cypress/e2e/cookies.test.ts
@@ -3,9 +3,17 @@
 /// <reference types="@testing-library/cypress" />
 
 describe(`Interact with Cookies`, () => {
-	it(`Check for Cookie Notification`, () => {
+	it(`Analytics Cookies should not be set`, () => {
 		cy.visit(`/`).assertRoute(`/`);
 
+		cy.getCookie("gatsby-gdpr-google-tagmanager").should("not.exist");
+
+		cy.getCookie("gatsby-gdpr-facebook-pixel").should("not.exist");
+
+		cy.getCookie("gatsby-gdpr-google-analytics").should("not.exist");
+	});
+
+	it(`Check for Cookie Notification`, () => {
 		cy.get(`#cookie-notice`).contains(`Accept`);
 
 		cy.get(`#cookie-notice`).contains(`Manage`);
@@ -39,12 +47,70 @@ describe(`Interact with Cookies`, () => {
 		cy.get(`.analyticsCookies-input input[type="checkbox"]`).should(
 			`not.be.checked`
 		);
+
+		cy.get(`.analyticsCookies-input`).click();
+
+		cy.get(`.analyticsCookies-input input[type="checkbox"]`).should(
+			`be.checked`
+		);
+
+		Cypress.Cookies.preserveOnce(
+			"gatsby-gdpr-google-tagmanager",
+			"gatsby-gdpr-facebook-pixel",
+			"gatsby-gdpr-google-analytics"
+		);
 	});
 
 	it(`Accept and close cookie preferences`, () => {
 		cy.get(`#manage-cookies footer button`).click();
 
 		cy.get(`#manage-cookies`).should(`not.exist`);
+
+		Cypress.Cookies.preserveOnce(
+			"gatsby-gdpr-google-tagmanager",
+			"gatsby-gdpr-facebook-pixel",
+			"gatsby-gdpr-google-analytics"
+		);
+	});
+
+	it(`Analytics Cookies should exist`, () => {
+		cy.getCookie("gatsby-gdpr-google-tagmanager").should("exist");
+
+		cy.getCookie("gatsby-gdpr-facebook-pixel").should("exist");
+
+		cy.getCookie("gatsby-gdpr-google-analytics").should("exist");
+
+		Cypress.Cookies.preserveOnce(
+			"gatsby-gdpr-google-tagmanager",
+			"gatsby-gdpr-facebook-pixel",
+			"gatsby-gdpr-google-analytics"
+		);
+	});
+
+	it(`Disable analtyics cookies`, () => {
+		cy.get(`#cookie-notice`).contains(`Manage`).click();
+
+		cy.get(`button.analyticsCookies`).click();
+
+		cy.get(`.analyticsCookies-input input[type="checkbox"]`).should(
+			`be.checked`
+		);
+
+		cy.get(`.analyticsCookies-input`).click();
+
+		cy.get(`.analyticsCookies-input input[type="checkbox"]`).should(
+			`not.be.checked`
+		);
+
+		cy.get(`#manage-cookies footer button`).click();
+
+		cy.get(`#manage-cookies`).should(`not.exist`);
+
+		cy.getCookie("gatsby-gdpr-google-tagmanager").should("not.exist");
+
+		cy.getCookie("gatsby-gdpr-facebook-pixel").should("not.exist");
+
+		cy.getCookie("gatsby-gdpr-google-analytics").should("not.exist");
 	});
 
 	it(`Accept cookies`, () => {
