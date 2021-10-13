3eec05a5e36bd89bfe7f4258a4c69f6b4692b560
Kaleb Heitzman <kaleb@jamfire.io>
2021-10-13 15:06:14 -0400

    refactor(gatsby-theme-jamfire-conference): updated event tracker

--- packages/gatsby-theme-jamfire-conference/src/components/site-header/header.d.ts
+++ packages/gatsby-theme-jamfire-conference/src/components/site-header/header.d.ts
@@ -4,6 +4,7 @@ import { MarkdownRemark, Maybe, File } from "../../gatsby/graphql-types"
 // header props
 export interface HeaderProps {
   config?: MarkdownRemark
+  event?: MarkdownRemark
   headerLogo?: Maybe<File>
   title: string | null
   locale: string
--- packages/gatsby-theme-jamfire-conference/src/components/site-header/header.tsx
+++ packages/gatsby-theme-jamfire-conference/src/components/site-header/header.tsx
@@ -1,6 +1,7 @@
 // import libs
 import React, { useContext } from "react"
 import { HeaderProps } from "./header.d"
+import loadable from "@loadable/component"
 
 // import components
 import { FirebaseContext } from "../../services"
@@ -8,10 +9,13 @@ import Avatar from "../avatar"
 import Branding from "../branding"
 import Topbar from "../topbar"
 
+// loadable components
+const Tracker = loadable(() => import("../event-tracker"))
+
 // import styles
 import * as styles from "./header.module.scss"
 
-export default ({ config, headerLogo, title }: HeaderProps) => {
+export default ({ config, event, headerLogo, title, locale }: HeaderProps) => {
   const { isLoading, isLoggedIn, profile } = useContext(FirebaseContext)
 
   return (
@@ -19,6 +23,7 @@ export default ({ config, headerLogo, title }: HeaderProps) => {
       <Avatar isLoading={isLoading} isLoggedIn={isLoggedIn} profile={profile} />
       <Branding config={config} headerLogo={headerLogo} title={title} />
       <Topbar isLoading={isLoading} isLoggedIn={isLoggedIn} config={config} />
+      {event && <Tracker event={event} locale={locale} />}
     </header>
   )
 }
