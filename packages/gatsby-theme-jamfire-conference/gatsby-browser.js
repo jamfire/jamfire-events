d154d4266c811d23bf7a791b38314617ee881c31
Kaleb Heitzman <kaleb@jamfire.io>
2021-11-10 13:01:51 -0500

    fix: 🐛 seo output

--- packages/gatsby-theme-jamfire-conference/gatsby-browser.js
+++ packages/gatsby-theme-jamfire-conference/gatsby-browser.js
@@ -47,3 +47,36 @@ export const wrapPageElement = ({ element, props: { pageContext } }) => {
     </LocalesProvider>
   )
 }
+
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  const headComponents = getHeadComponents()
+
+  const metaHeadComponents = headComponents.filter(
+    component => component.type === "meta"
+  )
+
+  const styleHeadComponents = headComponents.filter(
+    component => component.type === "style"
+  )
+
+  const scriptHeadComponents = headComponents.filter(
+    component => component.type === "script"
+  )
+
+  const nonStyleHeadComponents = headComponents.filter(
+    component =>
+      component.type !== "style" ||
+      component.type !== "script" ||
+      component.type !== "meta"
+  )
+
+  replaceHeadComponents([
+    metaHeadComponents,
+    styleHeadComponents,
+    scriptHeadComponents,
+    nonStyleHeadComponents,
+  ])
+}
